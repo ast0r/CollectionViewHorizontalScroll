28bfce0d881514f63754bd5c62ef1eb2e9134f4e
Pavel Ivanov <ivanovpavel@Pavels-Mac.local>
2019-03-14 21:09:00 +0200

    its a Iive!

--- CollectionViewTest2/ViewController.swift
+++ CollectionViewTest2/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var items: [Any] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        cell.contentView.backgroundColor = .green
+        
+        if let label = cell.contentView.viewWithTag(10) as? UILabel {
+            label.text = String(indexPath.row)
+        }
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.collectionView.frame.width)/2.0, height: (self.collectionView.frame.height)/4.0)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        items = [0,1,2,3,4,5,6,7,8]
     }
 
 
